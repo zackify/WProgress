512e76fdf042ae67a77eb746fc6b7a1b31aa767c
Zach Silveira <zackify@gmail.com>
2015-07-15 18:57:53 -0700

    changed color, only show on post page

--- actions.php
+++ actions.php
@@ -1,9 +1,9 @@
 <?php
 
 function WProgressJS() {
+  if(!is_singular()) return true;
 	wp_enqueue_style( 'wprogrecss', plugins_url( 'css/progress.css', __FILE__ ) );
   wp_enqueue_script('jquery');
 	wp_enqueue_script( 'wprogress', plugins_url( 'js/progress.js', __FILE__ ), array(), '0.1', true );
 }
-
-add_action( 'wp_enqueue_scripts', 'WProgressJS' );
+  add_action( 'wp_enqueue_scripts', 'WProgressJS' );
